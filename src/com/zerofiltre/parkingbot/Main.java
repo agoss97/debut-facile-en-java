233fbfe27779dd5aa1ab72cdb073b8f767b6561c
AGOSSOU Cyr <emery.a@denucom.com>
2025-12-27 23:36:59 +0100

    P1c2d2 OK

--- src/com/zerofiltre/parkingbot/Main.java
+++ src/com/zerofiltre/parkingbot/Main.java
@@ -7,7 +7,13 @@ public class Main {
        int length = 180;
        int width = 120;
        int surface = length * width;
+       int perimeter = (length + width) * 2;
+       int difference = length - width;
+       double ratio = (double) width / length;
        System.out.println("La surface au sol de la voiture est de: "+ surface);
+       System.out.println("Le périmètre de la voiture est de: "+ perimeter);
+       System.out.println("La difference entre la longueur et la largeur est de: "+ difference);
+       System.out.println("Le ratio entre la longueur et la largeur est de: "+ ratio);
 
     }
 }
