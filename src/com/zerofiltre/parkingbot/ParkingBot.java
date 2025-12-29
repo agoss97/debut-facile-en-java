789946fbfcebc0aebaa868ee0c798878405e099f
AGOSSOU Cyr <emery.a@denucom.com>
2025-12-29 02:46:52 +0100

    La portée des variables

--- src/com/zerofiltre/parkingbot/ParkingBot.java
+++ src/com/zerofiltre/parkingbot/ParkingBot.java
@@ -1,6 +1,11 @@
 package com.zerofiltre.parkingbot;
 
+import com.zerofiltre.parkingbot.service.ParkingService;
+
 public class ParkingBot {
+
+  static String completion = "des équipes du parking Zerofiltre";
+  static ParkingService parkingService = new ParkingService();
 
   /**
    * Ceci est la méthode Main
@@ -17,11 +22,14 @@ public class ParkingBot {
    * Cette méthode permet de dire Hello
    */
   private static void sayHello() {
-    String welcomeSentence = "Hello, recevez la bienvenue ";
+    String welcomeSentence = "Hello, recevez la bienvenue " + completion;
+    String parkService = parkingService.parking;
+    String washService = parkingService.washing;
     System.out.println(welcomeSentence);
+    System.out.println(parkService + " et " + washService);
   }
 
   private static void sayBye() {
-    System.out.println("Recevez les aurevoirs");
+    System.out.println("Recevez les aurevoirs " + completion);
   }
 }
---
+++ src/com/zerofiltre/parkingbot/service/ParkingService.java
@@ -0,0 +1,6 @@
+package com.zerofiltre.parkingbot.service;
+
+public class ParkingService {
+    public String parking = "Nous avons un service parking et aussi de location de véhicules";
+    public String washing = "nous offrons également un service de lavage de véhicules.";
+}
